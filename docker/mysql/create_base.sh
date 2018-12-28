e73b449753289f34731c1134febc184cbefb1ad3
akmua <akmua@mail.ru>
2018-12-28 07:50:50 +0200

    fixes

--- docker/mysql/create_base.sh
+++ docker/mysql/create_base.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
-echo "create database domain_statistic;" | mysql mysql;
-echo "GRANT ALL PRIVILEGES ON domain_statistic.* TO domain_statistic@'%' IDENTIFIED BY 'domain_statisticdomain_statistic';" | mysql mysql;
-echo "FLUSH PRIVILEGES;" | mysql mysql;
 MYPASSWD=$RANDOM$RANDOM$RANDOM
 mysqladmin -u root password $MYPASSWD;
-echo "[client]" > /root/.my.cnf;
-echo "password=$MYPASSWD" >> /root/.my.cnf;
-mysql domain_statistic < /root/structure.sql;
+
+mysql -uroot -p$MYPASSWD "CREATE DATABASE IF NOT EXISTS domain_statistic DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;"
+mysql -uroot -p$MYPASSWD "CREATE USER 'pma'@'%' IDENTIFIED BY 'pma';"
+mysql -uroot -p$MYPASSWD "GRANT ALL PRIVILEGES ON * . * TO 'pma'@'%';"
+mysql -uroot -p$MYPASSWD "FLUSH PRIVILEGES;"
+mysql -uroot -p$MYPASSWD "domain_statistic < /root/structure.sql;"
+touch /root/.my.cnf;
+echo "[client]" > /root/.my.cnf 
+echo "password=$MYPASSWD" >> /root/.my.cnf
 
 echo "Done"
